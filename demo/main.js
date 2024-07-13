119b3946e1ed207ffb15d20e8d4bd51295639e61
patriktoth67 <handleveled.net@gmail.com>
2024-07-13 12:01:32 +0200

    asd

--- demo/main.js
+++ demo/main.js
@@ -6,6 +6,7 @@ Yaplet.setApiUrl("http://localhost:3000/api");
 Yaplet.setBannerUrl("http://localhost:5173");
 //Yaplet.setWSApiUrl("ws://localhost:4000/socket/websocket");
 
-Yaplet.initialize("345fa619-c4d0-4893-8b1c-547808fe044f");
+Yaplet.initialize("345fa619-c4d0-4893-8b1c-547808fe044f"); // IG
+// Yaplet.initialize("3528f1f0-33a7-43d3-b334-c61ee682447c"); // TEST
 
 //Yaplet.showSurvey("241d5cd9-9e35-47e9-88fb-34943656832c", "survey_full");
