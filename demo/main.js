5400be5c94d9dde5a25da6de28adb2bf243575bf
patriktoth67 <handleveled.net@gmail.com>
2026-01-05 14:06:48 +0100

    fix

--- demo/main.js
+++ demo/main.js
@@ -8,9 +8,9 @@ Yaplet.setAdminUrl("http://localhost:3000");
 //Yaplet.setWSApiUrl("ws://localhost:4000/socket/websocket");
 
 // Yaplet.initialize("a22e7df8-7329-458a-ac18-16970543ff73"); // IG
-Yaplet.initialize("3528f1f0-33a7-43d3-b334-c61ee682447c"); // TEST
+// Yaplet.initialize("3528f1f0-33a7-43d3-b334-c61ee682447c"); // TEST
 // Yaplet.initialize("26930a7f-fa2b-4094-85c8-d583aa20c76e"); // Yaplet display
 // Yaplet.initialize("998782b5-8863-4c6e-b186-5f6f576bd9ec"); // Yaplet separate display acc
-("38e4449b-6c2d-4822-aedb-095ff83bdace"); // Tippmix
+Yaplet.initialize("38e4449b-6c2d-4822-aedb-095ff83bdace"); // Tippmix
 
 //Yaplet.showSurvey("241d5cd9-9e35-47e9-88fb-34943656832c", "survey_full");
